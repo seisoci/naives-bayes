559871ae0e2c6d85d267af2d30b78dc9a0a901ef
seisoci <63911558+seisoci@users.noreply.github.com>
2024-02-04 15:48:38 +0700

    ADD - Done Naives Bayes Web

--- routes/panel.php
+++ routes/panel.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('logout', [Panel\AuthController::class, 'logout'])->name('logout');
 Route::get('logout', [Panel\AuthController::class, 'logout'])->name('logout');
 
 Route::get('/', [Panel\AuthController::class, 'showLoginForm']);
